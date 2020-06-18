457ba2ebb3546db8ab2ed91e9ed5bfa6976dfe6c
Felipe de Almeida Silva <felipe.dealmeidasilva@ldc.com>
2020-06-18 16:56:31 +0200

    adding type of the object that passed

--- src/helpers/pass.js
+++ src/helpers/pass.js
@@ -1,10 +1,10 @@
-const chalk = require('chalk');
+let chalk = require('chalk');
 const log = console.log;
 
 module.exports = (data) => {
 	log(chalk.bold.green(' PASS '));
 	// @ToDo:  on verbose
 	data.SUMARIES.map(test => {
-		log(chalk.green('PASS: ') + test.NAME);
+		log(chalk.green('PASS: ') + test.TYPE + ' ' + test.NAME );
 	});
 };
